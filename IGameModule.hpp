d3d9c0cf21bf64aecfe37f3715fe3213fb6b2157
Matthieu <mattdaumas@hotmail.com>
2022-03-25 11:13:43 +0100

    (+) Add 2 methods in IGameModule + Change queue type of EventManager

--- IGameModule.hpp
+++ IGameModule.hpp
@@ -14,7 +14,12 @@
 #include "IDisplayModule.hpp"
 
 namespace Arcade {
-
+    enum SwitchStatus
+    {
+        PREV = -1,
+        NONE = 0,
+        NEXT = 1
+    };
     /*!
     ** \interface IGameModule
     ** This is an interface containing only pure virtual functions.
@@ -35,6 +40,9 @@ namespace Arcade {
         virtual std::map<char, struct Data> &getConfig(void) = 0;
         virtual void restart(void) = 0; ///< restarts the game
         virtual void pause(void) = 0; ///< pauses the game
+        virtual bool isQuit(void) const = 0; ///< Check if a quite signal has been sent
+        virtual SwitchStatus getGameSwitch(void) const = 0; ///< get game switch status
+        virtual SwitchStatus getDisplaySwitch(void) const = 0; ///< get display switch status
         ///< @return true if the KEYEXIT has been pressed
         ///< @see Keyboard for more information about the keys
         virtual void update(void) = 0; ///< this function updates the game data if needed
